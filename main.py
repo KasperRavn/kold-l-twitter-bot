d3b10479d7cdf3e8398168632cebe5a01d43e035
Kasper Ravn <kasperravn10@gmail.com>
2026-05-25 21:44:28 +0200

    Opdateret til at den nu tweeter klokken 13.05 hver dag

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ client = tweepy.Client(bearer_token, consumer_key, consumer_key_secret, access_t
 
 def lav_tweet(temp_klokken_tolv):
     if temp_klokken_tolv > 15:
-        client.create_tweet(text="xdd")
+        client.create_tweet(text="Ja det er en farlig dag at være en kold øl på")
     else:
         client.create_tweet(text="Nej i dag er ikke en farlig dag at være en kold øl på")
 
