6d8796837a9af87d8dc6529f0aa2a181c5a61937
Kasper Ravn <kasperravn10@gmail.com>
2026-05-25 21:25:57 +0200

    Færdiggjorde mit python script og tester nu med 5 minutters interval med github actions

--- main.py
+++ main.py
@@ -10,6 +10,23 @@ url = BASE_URL
 
 response = requests.get(url).json()
 temp_klokken_tolv = response['hourly']['temperature_2m'][12]
-temp_klokken_tolv_json = {"temperatur": temp_klokken_tolv}
 
-print(temp_klokken_tolv_json)
+load_dotenv()
+
+bearer_token = os.getenv("BEARER_TOKEN")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_key_secret = os.getenv("CONSUMER_KEY_SECRET")
+
+
+client = tweepy.Client(bearer_token, consumer_key, consumer_key_secret, access_token, access_token_secret)
+
+def lav_tweet(temp_klokken_tolv):
+    if temp_klokken_tolv > 15:
+        client.create_tweet(text="Ja i dag er en farlig dag at være en kold øl på")
+    else:
+        client.create_tweet(text="Nej i dag er ikke en farlig dag at være en kold øl på")
+
+
+lav_tweet(temp_klokken_tolv)
